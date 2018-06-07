ea0faf0d7ccddcec35140df8eb48f741c7d20f14
rudenko174 <rudenko.qa@gmail.com>
2018-06-07 09:11:31 +0300

    First Task after renames class and public voids 2

--- addressbook-web-tests/addressbook-web-tests/UnitTest2.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest2.cs
@@ -7,7 +7,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace WebAddressBookTests
+namespace WebAddressBookTests 
 {
     [TestFixture]
     public class GroupCreationTests
