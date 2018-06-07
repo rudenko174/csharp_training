680d791070be4de45d4335821e6b078372bf049d
rudenko174 <rudenko.qa@gmail.com>
2018-06-07 08:52:00 +0300

    First Task after renames class and public voids

--- addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace addressbook_web_tests
 {
     [TestClass]
-    public class UnitTest1
+    public class GroupCreationTests
     {
         [TestMethod]
         public void TestMethod1()
