054e9532e4a07403c64aba921f4b41612a6e722a
Руденко Дима <felix174@mail.ru>
2018-07-06 14:13:09 +0300

    Проверить работоспособность программы. Сделано 3е задание

--- addressbook-web-tests/addressbook-web-tests/AppManager/NavigationHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/NavigationHelper.cs
@@ -21,11 +21,20 @@ namespace WebAddressBookTests
 
         public void GoToHomePage()
         {
+            if (driver.Url == baseURL + "/addressbook/")
+            {
+                return;
+            }
             driver.Navigate().GoToUrl(baseURL + "/addressbook/");
         }
 
         public void GoToGroupsPage()
         {
+            if (driver.Url == baseURL + "/addressbook/group.php"
+                && IsElementPresent(By.Name("new")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("groups")).Click();
         }
     }
